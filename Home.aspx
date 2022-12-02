edd45d19e2b7ca48131b0bd118210afaab0a1ab5
zÉ <jose.rubens@etec.sp.gov.br>
2022-12-01 21:38:42 -0300

    jogadores colocados.

--- Home.aspx
+++ Home.aspx
@@ -47,16 +47,34 @@
 
             <div id="carrosel_subs">
                 <div id="players_subs">
-                </div>
+          </div>
             </div>
         </div>
 
         <div id="field">
-            <img src=".\Resources\field.png" alt="fieldimg" id="field_img" />
-            <img id="meiaofensa" src=".\Resources\player1.png" alt="meiaofensa" class="player" />
-            <img id="esqrofensa" src=".\Resources\player2.png" alt="esqrofensa" class="player" />
-            <img id="direofensa" src=".\Resources\player3.png" alt="direofensa" class="player" />
-             <img id="gol" src=".\Resources\player11.jpg" alt="gol" class="player" />
+              <img src=".\Resources\field.png" alt="fieldimg" id="field_img" />
+            <img id="meiaofensa" src=".\Resources\player1.png" alt="THOMAS MULLER" class="player" />
+            <asp:Label ID="Label7" runat="server" Text="THOMAS MULLER 13" class="label_players"></asp:Label>
+            <img id="esqrofensa" src=".\Resources\player2.png" alt="SARGE GNABRY" class="player" />
+            <asp:Label ID="Label8" runat="server" Text="SARGE GNABRY 10" class="label_players"></asp:Label>
+            <img id="direofensa" src=".\Resources\player3.png" alt="LEROY SANE" class="player" />
+            <asp:Label ID="Label9" runat="server" Text="SANÉ 19"  class="label_players"></asp:Label>
+            <img id="meioofensa" src=".\Resources\player4.png" alt="JAMAL MUSIALA" class="player" />
+            <asp:Label ID="Label10" runat="server" Text="MUSIALA 14"  class="label_players"></asp:Label>
+            <img id="volesquerdo" src=".\Resources\player5.png" alt="GUNDOGAN" class="player" />
+            <asp:Label ID="Label11" runat="server" Text="GUNDOGAN 21"  class="label_players"></asp:Label>
+            <img id="voldireito" src=".\Resources\player6.png" alt="GORETZKA" class="player" />
+            <asp:Label ID="Label12" runat="server" Text="GORETZKA 8"  class="label_players"></asp:Label>
+            <img id="latesquerdo" src=".\Resources\player7.png" alt="D.RAUM" class="player" />
+            <asp:Label ID="Label13" runat="server" Text="D.RAUM 3"  class="label_players"></asp:Label>
+            <img id="zagesquerdo" src=".\Resources\player8.jpg" alt="RUDIGER" class="player" />
+            <asp:Label ID="Label14" runat="server" Text="RUDIGER 2"  class="label_players"></asp:Label>
+            <img id="zagdireito" src=".\Resources\player9.PNG" alt="NICOLAS SUELE" class="player" />
+            <asp:Label ID="Label15" runat="server" Text="SUELE 15"  class="label_players"></asp:Label>
+            <img id="latdireito" src=".\Resources\player10.PNG" alt="KIMMICH" class="player" />
+            <asp:Label ID="Label16" runat="server" Text="KIMMICH 6"  class="label_players"></asp:Label>
+            <img id="goleiro" src=".\Resources\player11.jpg" alt="MANUEL NUER" class="player" />
+            <asp:Label ID="Label17" runat="server" Text="NUER 1"  class="label_players"></asp:Label>
         </div>
     </form>
 
--- Home.aspx.designer.cs
+++ Home.aspx.designer.cs
@@ -76,5 +76,104 @@ namespace copamundo
         /// Modificar a declaração do campo de movimento do arquivo de designer para o arquivo code-behind.
         /// </remarks>
         protected global::System.Web.UI.WebControls.Label Label6;
+
+        /// <summary>
+        /// Controle Label7.
+        /// </summary>
+        /// <remarks>
+        /// Campo gerado automaticamente.
+        /// Modificar a declaração do campo de movimento do arquivo de designer para o arquivo code-behind.
+        /// </remarks>
+        protected global::System.Web.UI.WebControls.Label Label7;
+
+        /// <summary>
+        /// Controle Label8.
+        /// </summary>
+        /// <remarks>
+        /// Campo gerado automaticamente.
+        /// Modificar a declaração do campo de movimento do arquivo de designer para o arquivo code-behind.
+        /// </remarks>
+        protected global::System.Web.UI.WebControls.Label Label8;
+
+        /// <summary>
+        /// Controle Label9.
+        /// </summary>
+        /// <remarks>
+        /// Campo gerado automaticamente.
+        /// Modificar a declaração do campo de movimento do arquivo de designer para o arquivo code-behind.
+        /// </remarks>
+        protected global::System.Web.UI.WebControls.Label Label9;
+
+        /// <summary>
+        /// Controle Label10.
+        /// </summary>
+        /// <remarks>
+        /// Campo gerado automaticamente.
+        /// Modificar a declaração do campo de movimento do arquivo de designer para o arquivo code-behind.
+        /// </remarks>
+        protected global::System.Web.UI.WebControls.Label Label10;
+
+        /// <summary>
+        /// Controle Label11.
+        /// </summary>
+        /// <remarks>
+        /// Campo gerado automaticamente.
+        /// Modificar a declaração do campo de movimento do arquivo de designer para o arquivo code-behind.
+        /// </remarks>
+        protected global::System.Web.UI.WebControls.Label Label11;
+
+        /// <summary>
+        /// Controle Label12.
+        /// </summary>
+        /// <remarks>
+        /// Campo gerado automaticamente.
+        /// Modificar a declaração do campo de movimento do arquivo de designer para o arquivo code-behind.
+        /// </remarks>
+        protected global::System.Web.UI.WebControls.Label Label12;
+
+        /// <summary>
+        /// Controle Label13.
+        /// </summary>
+        /// <remarks>
+        /// Campo gerado automaticamente.
+        /// Modificar a declaração do campo de movimento do arquivo de designer para o arquivo code-behind.
+        /// </remarks>
+        protected global::System.Web.UI.WebControls.Label Label13;
+
+        /// <summary>
+        /// Controle Label14.
+        /// </summary>
+        /// <remarks>
+        /// Campo gerado automaticamente.
+        /// Modificar a declaração do campo de movimento do arquivo de designer para o arquivo code-behind.
+        /// </remarks>
+        protected global::System.Web.UI.WebControls.Label Label14;
+
+        /// <summary>
+        /// Controle Label15.
+        /// </summary>
+        /// <remarks>
+        /// Campo gerado automaticamente.
+        /// Modificar a declaração do campo de movimento do arquivo de designer para o arquivo code-behind.
+        /// </remarks>
+        protected global::System.Web.UI.WebControls.Label Label15;
+
+        /// <summary>
+        /// Controle Label16.
+        /// </summary>
+        /// <remarks>
+        /// Campo gerado automaticamente.
+        /// Modificar a declaração do campo de movimento do arquivo de designer para o arquivo code-behind.
+        /// </remarks>
+        protected global::System.Web.UI.WebControls.Label Label16;
+
+        /// <summary>
+        /// Controle Label17.
+        /// </summary>
+        /// <remarks>
+        /// Campo gerado automaticamente.
+        /// Modificar a declaração do campo de movimento do arquivo de designer para o arquivo code-behind.
+        /// </remarks>
+        protected global::System.Web.UI.WebControls.Label Label17;
     }
 }
